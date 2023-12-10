26c64c0856ae68ec52feb148c15ec177f69ad3b5
Juampi27 <jpbarnetche27@gmail.com>
2023-12-10 00:40:40 -0300

    visualizacion de usuarios y algunos estilos.

---
+++ app/page.js
@@ -0,0 +1,25 @@
+
+async function fetchUsers() {
+  const res = await fetch('https://reqres.in/api/users')
+  const data = await res.json()
+  return data.data
+}
+
+export default async function HomePage() {
+
+  const users = await fetchUsers()
+
+  return (
+    <ul>
+      {users.map(user =>(
+        <li key={user.id} className="bg-slate-400 mb-2 p-4 rounded-md text-black flex justify-between">
+          <div>
+            <h5 className="font-bold">{user.id} {user.first_name} {user.last_name}</h5>
+            <p className="text-slate-100">email: {user.email}</p>
+          </div>
+          <img src={user.avatar} className="rounded-full w-20" alt="" />
+        </li>
+      ))}
+    </ul>
+  )
+}
